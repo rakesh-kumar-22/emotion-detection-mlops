0a3adb0980272a516493a211e4f21ddb64f4f2eb
Rakesh Kumar Mohanty <rkumar36462@gmail.com>
2025-01-29 20:03:33 +0530

    after some error rectified

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -106,7 +106,7 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
         raise
 
 def main():
-    mlflow.set_experiment("dvc-pipeline")
+    mlflow.set_experiment("dvc-pipeline2")
     with mlflow.start_run() as run:  # Start an MLflow run
         try:
             clf = load_model('./models/model.pkl')
