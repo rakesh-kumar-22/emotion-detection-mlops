f66c2c842a9eca1b7b957b3761c52ac3fd9c8a89
Rakesh Kumar Mohanty <rkumar36462@gmail.com>
2025-01-30 17:22:39 +0530

    add model signature test

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,6 +3,8 @@
 import unittest
 import mlflow
 import os
+import pickle
+import pandas as pd
 
 class TestModelLoading(unittest.TestCase):
 
@@ -28,7 +30,8 @@ class TestModelLoading(unittest.TestCase):
         cls.model_version = cls.get_latest_model_version(cls.new_model_name)
         cls.model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
         cls.model = mlflow.pyfunc.load_model(cls.new_model_uri)
-        
+        #vectorizer 
+        cls.vectorizer=pickle.load(open('models/vectorizer.pkl','rb'))
         @staticmethod
         def get_latest_model_version(model_name):
             client = mlflow.tracking.MlflowClient()
@@ -36,6 +39,20 @@ class TestModelLoading(unittest.TestCase):
             return latest_version[0].version if latest_version else None
         def test_model_loaded_properly(self):
             self.assertIsNotNone(self.model)
+            
+        def test_model_signature(self):
+            #create a dummy input for the model based on input shape
+            input_text= "hi how are you"
+            input_data= self.vetorize.transform([input_text])
+            input_df=pd.dataframe(input_data.to_array(),columns=[str(i) for i in range(input_data.shape[1])])
+            #predict using the model to check input and output shape
+            prediction=self.model.predict(input_df)
+            
+            #verify the input shape
+            self.assertEqual(input_df.shape[1], len(self.vectorizer.get_feature_names_out()))
+            self.assertEqual(len(prediction),input_df.shape[0])
+            #verify the output shape
+            self.assertEqual(len(prediction.shape),1)
             
 if __name__ == '__main__':
     unittest.main()
